f45c0229093d0456a62e3ebf5198614d33442df6
Malikbasha <148067076+Malikbasha4@users.noreply.github.com>
2024-01-20 22:32:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import SignUp from './SignUp';
 import Login from './Login';
 import Profile from './Profile';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <Router>
       <Switch>
         <Route path="/signup" component={SignUp} />
-        <Route path="/login" component={Login} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/login">
+          <Login onLogin={handleLogin} />
+        </Route>
+        <PrivateRoute path="/profile" component={Profile} isLoggedIn={isLoggedIn} />
+        <Redirect from="/" to="/login" />
       </Switch>
     </Router>
   );
 };
+
+const PrivateRoute = ({ component: Component, isLoggedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isLoggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
 
 export default App;
